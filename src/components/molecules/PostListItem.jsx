2db5bbc8b3a315a82623c011787fd5003978cc3c
llmsilva <86919260+llmsilva@users.noreply.github.com>
2022-10-13 22:20:50 -0300

    Adiciona página de blog do usuário

---
+++ src/components/molecules/PostListItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function PostListItem(props) {
+  return (
+    <div className="user-blog__posts-item">
+      <div className="user-blog__posts-item-photo">
+        <img src={props.image} className="responsive" alt="" />
+      </div>
+      <h2 className="user-blog__posts-item-title">{props.title}</h2>
+      <div className="user-blog__posts-date">
+        Publicado em {props.createdAt}
+      </div>
+      <div className="user-blog__post-content">{props.content}</div>
+    </div>
+  );
+}
