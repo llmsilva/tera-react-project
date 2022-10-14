abc7d44e719507db2a0e3aec8081dd0fec9ed847
llmsilva <86919260+llmsilva@users.noreply.github.com>
2022-10-13 21:34:42 -0300

    Adiciona Navegação

--- src/components/organisms/AppHeader.jsx
+++ src/components/organisms/AppHeader.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../images/logo.svg";
 
-export default function AppHeader() {
+export default function AppHeader(props) {
   return (
     <div className="app-header">
       <div className="app-header__logo">
-        <img src={logo} className="responsive" alt="" />
+        <Link to="/">
+          <img src={logo} className="responsive" alt="" />
+        </Link>
       </div>
       <div className="app-header__menu">
-        <i className="fa fa-bars"></i>
+        <i onClick={() => props.setOpen(true)} className="fa fa-bars"></i>
       </div>
     </div>
   );
